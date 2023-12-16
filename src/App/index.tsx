b4234a8f58cd099bea4970b7028e83caa4e6bfe7
raihanalmenata <raihanalmenata@gmail.com>
2023-12-17 01:44:53 +0700

    Added app & app container

---
+++ src/App/index.tsx
@@ -0,0 +1,17 @@
+import '../designs/styles/color.css'
+import '../designs/styles/typography.css'
+import '../designs/styles/other.css'
+
+import './styles.css'
+
+const App = () => {
+  return (
+    <div id="app-con">
+      <div id="app">
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
