426b6501b54f1d0047f27ce22ba2adb0413caa6c
raihanalmenata <raihanalmenata@gmail.com>
2023-12-17 02:00:26 +0700

    Added app header

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,11 +3,22 @@ import '../designs/styles/typography.css'
 import '../designs/styles/other.css'
 
 import './styles.css'
+import icon from '../assets/icon.svg'
 
 const App = () => {
   return (
     <div id="app-con">
       <div id="app">
+        <header id="app-header">
+          <div id="icon-con">
+            <img src={icon} alt="" id="icon" />
+          </div>
+          <div id="heading-con">
+            <h2 id="subtitle">Data Visualization Dashboard:</h2>
+            <h1 id="title">USGS Recorded US Earthquake</h1>
+            <p id="desc">Any brief description here..</p>
+          </div>
+        </header>
       </div>
     </div>
   )
